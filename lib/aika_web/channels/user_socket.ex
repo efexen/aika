a59ce9e20d42bb3cd0fbff751485c5b707a1ada8
efexen <fxn@fxndev.com>
2017-10-22 19:37:15 +0100

    Heroku config

--- lib/aika_web/channels/user_socket.ex
+++ lib/aika_web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule AikaWeb.UserSocket do
   # channel "room:*", AikaWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
