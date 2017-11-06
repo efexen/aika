fe2a1f67e27098df7169f096bb748c02d4025e5b
Oliver Herst <oliver.herst@googlemail.com>
2017-11-06 21:55:20 +0000

    replace brunch with webpack

---
+++ assets/webpack.config.js
@@ -0,0 +1,61 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: {
+    app: "./js/app.js",
+    homepage: "./js/homepage.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "../priv/static"),
+    filename: "js/[name].js"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: __dirname,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env", "stage-3"]
+          }
+        }
+      },
+      {
+        test: /\.(sass|scss)$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "resolve-url-loader", "sass-loader?sourceMap"]
+        })
+      },
+
+      {
+        test: /\.(jpg|jpeg|gif|png|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10000
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  resolve: {
+    modules: ["node_modules", __dirname + "/web/static/js"]
+  },
+
+  plugins: [
+    new ExtractTextPlugin("css/[name].css"),
+    new CopyWebpackPlugin([
+      {from: "./static", to: path.resolve(__dirname, "../priv/static")}
+    ])
+  ]
+};
